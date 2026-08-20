784ca18e3a36bcabbad59ca963b1f6e4eda147a3
童顺 <15730642468@163.com>
2026-08-20 23:33:27 +0800

    Initialize GUI module with necessary imports

---
+++ client/gui/__init__.py
@@ -0,0 +1,7 @@
+"""客户端图形界面模块。"""
+from .main_window import MainWindow
+from .register import RegisterWindow
+from .chat_panel import ChatPanel
+from .contact_list import ContactList
+from .settings import SettingsWindow
+from .admin_panel import AdminPanel
